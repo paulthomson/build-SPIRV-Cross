14fb3e275dd66e9363fa1f65c6d72f686857e8b6
Paul Thomson <pault543@gmail.com>
2017-08-19 21:27:10 +0100

    Try removing shadow warning flag.

--- 2-build-travis.sh
+++ 2-build-travis.sh
@@ -9,9 +9,14 @@ INSTALL_DIR="${BUILD_PLATFORM}-${CMAKE_BUILD_TYPE}"
 BUILD_DIR="${INSTALL_DIR}-build"
 
 
+if [ "$(uname)" == "Linux" ];
+then
+  sed -i 's/-Wshadow//g' CMakeLists.txt
+fi
+
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
-cmake -G "${CMAKE_GENERATOR}" .. "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" ${CMAKE_OPTIONS}
+cmake -G "${CMAKE_GENERATOR}" .. "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" -DCMAKE_CXX_STANDARD=11 ${CMAKE_OPTIONS}
 cmake --build . --config "${CMAKE_BUILD_TYPE}"
 cmake "-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}" "-DBUILD_TYPE=${CMAKE_BUILD_TYPE}" -P cmake_install.cmake
 cd ..
