186b30e8c363b0dba722a0667aebaab1fc90b551
Paul Thomson <pault543@gmail.com>
2017-08-19 13:26:09 +0100

    Initial commit.

---
+++ 1-install-deps-appveyor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+mkdir temp
+cd temp
+
+GITHUB_RELEASE_TOOL_USER="paulthomson"
+GITHUB_RELEASE_TOOL_VERSION="v1.0.9.1"
+GITHUB_RELEASE_TOOL_ARCH="windows_amd64"
+
+
+curl -fsSL -o github-release.tar.gz https://github.com/${GITHUB_RELEASE_TOOL_USER}/github-release/releases/download/${GITHUB_RELEASE_TOOL_VERSION}/github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz
+
+7z x github-release.tar.gz
+7z x github-release.tar
+cd ..
+
+git clone https://github.com/KhronosGroup/SPIRV-Cross.git
+cd SPIRV-Cross
+git checkout $(cat ../COMMIT_ID)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# build-SPIRV-Cross
